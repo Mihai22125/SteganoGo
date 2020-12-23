2cd70f6f6ecd192a337f1938e07aa861ace1d6e0
Mihai22125 <mihai.mtb10@gmail.com>
2020-12-23 16:54:44 +0200

    added IHDR types

--- source/pkg/png/consts.go
+++ source/pkg/png/consts.go
@@ -7,14 +7,14 @@ var pngHeader = []byte{137, 80, 78, 71, 13, 10, 26, 10}
 // Critical chunk types
 
 // must be the first chunk
-var typeIHDR = []byte("IHDR")
+var TypeIHDR = []byte("IHDR")
 
 //  contains the palette: a list of colors
-var typePLTE = []byte("PLTE")
+var TypePLTE = []byte("PLTE")
 
 // contains the image, which may be split among multiple IDAT chunks
 // The IDAT chunk contains the actual image data, which is the output stream of the compression algorithm
-var typeIDAT = []byte("IDAT")
+var TypeIDAT = []byte("IDAT")
 
 // marks the image end; the data field of the IEND chunk has 0 bytes/is empty
-var typeIEND = []byte("IEND")
+var TypeIEND = []byte("IEND")
