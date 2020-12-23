64da4f211ddb52b3c759bfa75f51c16b55a12380
Mihai22125 <mihai.mtb10@gmail.com>
2020-12-23 16:45:32 +0200

    added a  function for comparing two png IHDR

--- source/pkg/png/utils.go
+++ source/pkg/png/utils.go
@@ -1,5 +1,7 @@
 package png
 
+import "bytes"
+
 // converts uint32 value to byte slice
 func i32ToB(val uint32) []byte {
 	r := make([]byte, 4)
@@ -7,4 +9,12 @@ func i32ToB(val uint32) []byte {
 		r[3-i] = byte((val >> (8 * i)) & 0xff)
 	}
 	return r
+}
+
+// CompareType compares uint32 type value with another defined png type
+func CompareType(val uint32, pngType []byte) bool {
+	if bytes.Equal(i32ToB(val), pngType) {
+		return true
+	}
+	return false
 }
