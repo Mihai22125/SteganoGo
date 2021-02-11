aac8c66fd177840bd4b03b39f0b53f1ce7ca58f0
Mihai22125 <mihai.mtb10@gmail.com>
2021-02-11 15:49:54 +0200

    added some error messages

--- source/pkg/png/errors.go
+++ source/pkg/png/errors.go
@@ -7,3 +7,12 @@ var ErrBadPNG error = errors.New("PNG file format is not correct")
 
 // ErrNotPNG ..
 var ErrNotPNG error = errors.New("Given file is not a PNG file")
+
+// ErrIHDRMissing IHDR chunk is missing
+var ErrIHDRMissing error = errors.New("IHDR chunk is missing")
+
+// ErrIDATMissing IHDR chunk is missing
+var ErrIDATMissing error = errors.New("IDAT chunk is missing")
+
+// ErrInvalidIHDR invalid IHDR chunk
+var ErrInvalidIHDR error = errors.New("IHDR chunk is not valid")
